4dd603c03a6d9835f592d951ce2ebf0a1cbea6cd
Aida <galvezaida3@gmail.com>
2025-12-18 16:28:58 -0600

    index html

--- portafolio/src/App.jsx
+++ portafolio/src/App.jsx
@@ -1,11 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useContext } from "react";
 import Indice from "./components/Indice";
 import Resumen from "./components/Proyectos/Resumen";
 import Experiencia  from "./components/Experiencia/Experiencia";
 import ProyectoDetalle from "./components/Proyectos/ProyectoDetalle";
 import ScrollToTop from "./components/ScrollToTop";
+import { LanguageContext } from "./context/LanguageContext";
 
 export default function App() {
+
+    const { language } = useContext(LanguageContext);
+
+  useEffect(() => {
+    const titles = {
+      es: "Aída Gálvez | Portafolio",
+      en: "Aída Gálvez | Portfolio",
+    };
+    document.title = titles[language];
+  }, [language]);
+
   return (
     <BrowserRouter>
       <ScrollToTop />
